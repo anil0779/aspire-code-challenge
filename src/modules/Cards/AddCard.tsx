9aa46f1299c00cad0fd6882ea9ff1d94867f6c69
anilk-s <anil.kumar@sequoia.com>
2024-03-11 11:21:41 +0530

    fix: flicker on btn click

--- src/modules/Cards/AddCard.tsx
+++ src/modules/Cards/AddCard.tsx
@@ -19,9 +19,15 @@ const AddCard: React.FC<AddCardProps> = ({ onSubmit, onClose }) => {
   return (
     <Modal isOpen onClose={onClose}>
       <TitleModal className="title-modal">Add new Card</TitleModal>
-      <div className="flex" style={{ gap: "8px" }}>
-        <label htmlFor="cardName">Card name:</label>
+      <div className="flex flex-column" style={{ gap: "4px" }}>
+        <label
+          style={{ width: "100%", fontSize: "14px", fontWeight: "bold" }}
+          htmlFor="cardName"
+        >
+          Card name:
+        </label>
         <input
+          style={{ width: "100%", height: "32px" }}
           type="text"
           id="cardName"
           name="cardName"
@@ -34,9 +40,19 @@ const AddCard: React.FC<AddCardProps> = ({ onSubmit, onClose }) => {
       <br />
       <div className="center">
         <input
-          style={{ padding: "4px 8px", borderRadius: "4px", cursor: "pointer" }}
+          style={{
+            padding: "4px 8px",
+            borderRadius: "4px",
+            cursor: "pointer",
+            background: "#0c365a",
+            color: "#FFF",
+            height: "40px",
+            width: "96px",
+            fontSize: "16px",
+            fontWeight: "bold",
+          }}
           type="button"
-          value="submit"
+          value="Submit"
           disabled={!name?.length}
           onClick={() => {
             onSubmit({ name });
