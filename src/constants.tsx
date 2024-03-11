5b79ff70a1df7a1d3d9bf65225153c36d3df70e5
anilk-s <anil.kumar@sequoia.com>
2024-03-11 10:38:00 +0530

    fix: cosmetic changes

--- src/constants.tsx
+++ src/constants.tsx
@@ -68,7 +68,7 @@ export const MOCK_CARD_CREATION_DATA = [
   {
     bank: "ICICI",
     bankLogo:
-      "https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:80c753f2-db2f-4dfc-b6c2-ce39a4c787f0;revision=0?component_id=8584ee76-6533-4a12-836f-44e095a92acf&api_key=CometServer1&access_token=1710003816_urn%3Aaaid%3Asc%3AUS%3A80c753f2-db2f-4dfc-b6c2-ce39a4c787f0%3Bpublic_931775a6de535e2753811aad793ece803f7d5c81",
+      "https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:80c753f2-db2f-4dfc-b6c2-ce39a4c787f0;revision=0?component_id=8584ee76-6533-4a12-836f-44e095a92acf&api_key=CometServer1&access_token=1710175834_urn%3Aaaid%3Asc%3AUS%3A80c753f2-db2f-4dfc-b6c2-ce39a4c787f0%3Bpublic_034b26ae5c1ec5929fa0f790937bafb737687e0b",
     type: "VISA",
     typeLogo:
       "https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:80c753f2-db2f-4dfc-b6c2-ce39a4c787f0;revision=0?component_id=99a641f4-0323-46a8-918e-3e232548ba51&api_key=CometServer1&access_token=1710003816_urn%3Aaaid%3Asc%3AUS%3A80c753f2-db2f-4dfc-b6c2-ce39a4c787f0%3Bpublic_931775a6de535e2753811aad793ece803f7d5c81",
@@ -84,7 +84,7 @@ export const MOCK_CARD_CREATION_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/2/28/HDFC_Bank_Logo.svg",
     type: "Master",
     typeLogo:
-      "https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:80c753f2-db2f-4dfc-b6c2-ce39a4c787f0;revision=0?component_id=99a641f4-0323-46a8-918e-3e232548ba51&api_key=CometServer1&access_token=1710003816_urn%3Aaaid%3Asc%3AUS%3A80c753f2-db2f-4dfc-b6c2-ce39a4c787f0%3Bpublic_931775a6de535e2753811aad793ece803f7d5c81",
+      "https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:80c753f2-db2f-4dfc-b6c2-ce39a4c787f0;revision=0?component_id=8584ee76-6533-4a12-836f-44e095a92acf&api_key=CometServer1&access_token=1710175834_urn%3Aaaid%3Asc%3AUS%3A80c753f2-db2f-4dfc-b6c2-ce39a4c787f0%3Bpublic_034b26ae5c1ec5929fa0f790937bafb737687e0b",
     thru: "08/32",
     maskedCvv: "***",
     maskedNumber: "**** **** **** 6666",
